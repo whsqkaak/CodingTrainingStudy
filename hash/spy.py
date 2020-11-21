57dff1fe76204f7c938b012310926a6851bed43d
whsqkaak <whsqkaak@naver.com>
2020-11-21 14:16:22 +0900

    hash/spy.py

--- hash/spy.py
+++ hash/spy.py
@@ -1,5 +1,5 @@
 def solution(clothes):
-    answer = len(clothes)
+    answer = 1
     cloth_types = dict()
     
     for cloth in clothes:
@@ -7,12 +7,8 @@ def solution(clothes):
             cloth_types[cloth[1]] += 1
         else:
             cloth_types[cloth[1]] = 1
-    print(cloth_types)
-    tmp = 1
+            
     for cloth_type in cloth_types:
-        tmp *= cloth_types[cloth_type]
-    answer += tmp
+        answer *= cloth_types[cloth_type] + 1
+    answer -= 1
     return answer
-
-if __name__ == "__main__":
-    print(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]))
